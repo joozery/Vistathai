aa7f6ee0d847fc981498c0149205b8d0b24fe388
joozery <136419786+joozery@users.noreply.github.com>
2025-02-04 09:33:59 +0700

    อัพเดภาพ

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../../assets/logo2.png";
+import logo from "../../assets/logo2v.png";
 import LanguageSwitcher from "../LaguageSwitcher";
 
 const Navbar = () => {
