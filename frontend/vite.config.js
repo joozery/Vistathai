d76c1dbc9b20bbe97968691d139c440cb3ca5d11
joozery <136419786+joozery@users.noreply.github.com>
2025-02-04 20:40:47 +0700

    อัพฟ้อนในinsexhtml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    target: 'es2017', // รองรับ Safari รุ่นเก่า
+  },
+  server: {
+    headers: {
+      'Access-Control-Allow-Origin': '*', // แก้ไข CORS ปัญหาโหลดไฟล์ภายนอก
+    },
+  },
+  build: {
+    target: ['es2017', 'safari11'], // รองรับ Safari เวอร์ชันเก่า
+    minify: 'esbuild', // ใช้ esbuild เพื่อลดปัญหา Compatibility
+  },
+});
